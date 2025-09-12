69453e366603efb7b3a613ade76f32c83c7883d3
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-09-13 05:18:53 +0900

    Implement QuickSelect for median finding

--- Find-the-Median.cs
+++ Find-the-Median.cs
@@ -14,6 +14,14 @@ using System;
 
 class Result
 {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
     static Random rand = new Random();
     
     static void Swap(List<int> arr, int i, int j)
@@ -21,7 +29,48 @@ class Result
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-    }    
+    }
+
+    static int Partition(List<int> arr, int left, int right)
+    {
+        int pivotIndex = left + rand.Next(right - left + 1);
+        Swap(arr, pivotIndex, right);
+
+        int pivot = arr[right];
+        int i = left;
+
+        for (int j = left; j < right; j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                Swap(arr, i, j);
+                i++;
+            }
+        }
+        Swap(arr, i, right);
+        return i;
+    }
+
+    static int QuickSelect(List<int> arr, int left, int right, int k)
+    {
+        if (left == right)
+            return arr[left];
+
+        int pivotIndex = Partition(arr, left, right);
+
+        if (pivotIndex == k)
+        {
+            return arr[pivotIndex];
+        }
+        else if (pivotIndex > k)
+        {
+            return QuickSelect(arr, left, pivotIndex - 1, k);
+        }
+        else
+        {
+            return QuickSelect(arr, pivotIndex + 1, right, k);
+        }
+    }
 
     public static int findMedian(List<int> arr)
     {
@@ -29,8 +78,15 @@ class Result
         int midIndex = n / 2;
         return QuickSelect(arr, 0, n - 1, midIndex);
     }
-    
+
+
+    public static int findMedian2(List<int> arr)
+    {
+        arr.Sort();
+        return arr[arr.Count / 2];
+    }
 }
+
 
 class Solution
 {
