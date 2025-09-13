e8fba3b1cce2a65ba29d57e7d4e7e1db2e3d1805
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-09-14 08:13:51 +0900

    Create findMedian function skeleton in Result class

--- Find-the-Median.java
+++ Find-the-Median.java
@@ -9,3 +9,19 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+lass Result {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+    // Write your code here
+
+    }
+
+}
